b320d21cfde56a9061521f763ade8cb96d2d376f
meng <mz.zhoumeng@gmail.com>
2018-11-17 15:38:23 -0800

    Begin creating profile section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ShotChart } from "./ShotChart";
+import { Profile } from "./Profile";
 
 export class Main extends React.Component {
   render() {
     return (
-      <div>
+      <div className="main">
+        <Profile/>
         {/* playerId from mpn nba package - Vince Carter used for instance */}
         <ShotChart playerId={1713}/>
       </div>
