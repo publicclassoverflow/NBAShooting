a12b734e2297fc115b3183bfc5bd0054e2ab51f8
meng <mz.zhoumeng@gmail.com>
2018-12-19 01:09:04 -0800

    Updates

---
+++ precache-manifest.bca84cc2630fe4c07a07f7960fb7e0d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9470e2d5c6c7f425edc8e9d8baebdb05",
+    "url": "/NBAShooting/static/media/logo.9470e2d5.svg"
+  },
+  {
+    "revision": "770a264a4bc612581b45",
+    "url": "/NBAShooting/static/js/runtime~main.770a264a.js"
+  },
+  {
+    "revision": "e4460517d34d76f52878",
+    "url": "/NBAShooting/static/js/main.e4460517.chunk.js"
+  },
+  {
+    "revision": "f2b6d42277b256869e71",
+    "url": "/NBAShooting/static/js/1.f2b6d422.chunk.js"
+  },
+  {
+    "revision": "e4460517d34d76f52878",
+    "url": "/NBAShooting/static/css/main.bfc8850b.chunk.css"
+  },
+  {
+    "revision": "f2b6d42277b256869e71",
+    "url": "/NBAShooting/static/css/1.3cb9f158.chunk.css"
+  },
+  {
+    "revision": "2cd74749d2d6a89699c2be1a08ce6abb",
+    "url": "/NBAShooting/index.html"
+  }
+];
